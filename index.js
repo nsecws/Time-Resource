274949e988bab812a4fc9618b8ce234fbfbba13e
Joshua <scott.joshua@gmail.com>
2017-11-28 10:16:41 -0700

    inital install

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Denver");
     callback(null, {
         statusCode: '200',
+        headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers":"Content-Type, client_id, client_secret"},
         body:  JSON.stringify({ time:currentTime.getTime()}),
     });
 };
+
+exports.options = (event, context, callback) => {
+    callback(null, {
+        statusCode: '200',
+        headers: {"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Methods": "OPTIONS, GET", "Access-Control-Allow-Headers":"Content-Type, client_id, client_secret"},
+    });
+};
+
